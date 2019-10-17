83a8ce417d362b1042a81e60a55f38c67f8e54f7
BuildTools <unconfigured@null.spigotmc.org>
2019-10-17 22:32:12 +0200

    *allowed hosts

--- wedding/settings.py
+++ wedding/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', 'hochzeit.kritten.org']
+ALLOWED_HOSTS = ['localhost', 'api.hochzeit.kritten.org']
 
 
 # Application definition
