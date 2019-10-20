9ad952bbfe7d4ac0215b1c87385bc8af4bdf7d6b
BuildTools <unconfigured@null.spigotmc.org>
2019-10-20 14:59:59 +0200

    *improved admin page

--- api/admin.py
+++ api/admin.py
@@ -2,15 +2,34 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+from django.contrib.auth.forms import UserCreationForm
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('extern', 'count')
+
 
 class UserAdminCustom(UserAdmin):
+    add_form = CustomUserCreationForm
+
     fieldsets = (
         (None, {
             'fields': ('username', 'password')
         }),
         ('Info', {
             'fields': ('extern', 'count')
-        })
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'fields': ('username', 'password1', 'password2')
+        }),
+        ('Info', {
+            'fields': ('extern', 'count')
+        }),
     )
 
 
