8eef532ccbc3201448fa89d82af4de50a7780c28
BuildTools <unconfigured@null.spigotmc.org>
2019-12-01 11:11:56 +0100

    *deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "$0")"
-
 git pull
 
 cd frontend
-
 npm i
-
 npm run build
 
-cp base.htaccess dist/.htaccess
-
 cd ..
-
 python3.6 manage.py migrate
 
+cd frontend
+rm -r dist
+mkdir dist
+cp base.htaccess dist/.htaccess
+cp -r dist_tmp dist
+
+cd ..
 touch wedding/wsgi.py
 
