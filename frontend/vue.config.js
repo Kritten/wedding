a6e8871a149c84447d8e285b1506e4a88d96847c
BuildTools <unconfigured@null.spigotmc.org>
2019-12-01 11:07:35 +0100

    *output path

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   transpileDependencies: [
     'vuetify',
@@ -11,4 +13,6 @@ module.exports = {
       enableInSFC: false,
     },
   },
+
+  outputDir: path.resolve(__dirname, 'dist_tmp'),
 };
